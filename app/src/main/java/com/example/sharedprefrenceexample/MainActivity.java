50579bcc23abcdb681ed03fc9141f53d893d964f
Sudha Shanker Prasad <anubis1191@gmail.com>
2020-03-25 22:24:33 +0530

    Shared Preference

---
+++ app/src/main/java/com/example/sharedprefrenceexample/MainActivity.java
@@ -0,0 +1,68 @@
+package com.example.sharedprefrenceexample;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+// to find Shared Preference are saved go to
+// Device File Explorer --> data --> data -->
+// "Your app package name " (like my app project name is com.example.sharedprefrenceexample)--> shared_prefs-->myrefernce NAME(mypref)
+public class MainActivity extends AppCompatActivity {
+    SharedPreferences sharedpreferences;
+    TextView name;
+    TextView email;
+    public static final String mypreference = "mypref";
+    public static final String Name = "nameKey";
+    public static final String Email = "emailKey";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        name = findViewById(R.id.etName);
+        email = findViewById(R.id.etEmail);
+        sharedpreferences = getSharedPreferences(mypreference,
+                Context.MODE_PRIVATE);
+        if (sharedpreferences.contains(Name)) {
+            name.setText(sharedpreferences.getString(Name, ""));
+        }
+        if (sharedpreferences.contains(Email)) {
+            email.setText(sharedpreferences.getString(Email, ""));
+
+        }
+    }
+
+    public void Save(View view) {
+        String n = name.getText().toString();
+        String e = email.getText().toString();
+        SharedPreferences.Editor editor = sharedpreferences.edit();
+        editor.putString(Name, n);
+        editor.putString(Email, e);
+        editor.commit();
+    }
+
+    public void clear(View view) {
+        name.setText("");
+        email.setText("");
+
+    }
+
+    public void Get(View view) {
+        sharedpreferences = getSharedPreferences(mypreference,
+                Context.MODE_PRIVATE);
+
+        if (sharedpreferences.contains(Name)) {
+            name.setText(sharedpreferences.getString(Name, ""));
+        }
+        if (sharedpreferences.contains(Email)) {
+            email.setText(sharedpreferences.getString(Email, ""));
+
+        }
+    }
+
+    }
+
+
